8460db3b1cecf2f1b16156a0bb78012d16bd066f
mrroot98 <42290222+mrroot98@users.noreply.github.com>
2019-08-20 00:58:28 +0530

    about us file uploaded

---
+++ travel/aboutus.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>About Us</title>
+	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css">
+	<script type="text/javascript" src="bootstrap/js/jquery.js"></script>
+	<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+	<div>
+		<div>
+			<img src="img/aboutusbanner.jpg" style="height: 700px; width: 100%;">
+			<div>
+				<h1 style="font-size: 5.5em; position: absolute; top: 20%; left: 40%;color: white; ">About Us</h1>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
