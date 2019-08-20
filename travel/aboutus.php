8865a57a37066797a1102150abc80b1ed0d76b7f
mrroot98 <42290222+mrroot98@users.noreply.github.com>
2019-08-21 00:30:59 +0530

    header and footer added

--- travel/aboutus.php
+++ travel/aboutus.php
@@ -10,6 +10,32 @@
 </head>
 <body>
 	<!-- Header File -->
+	<nav class="navbar navbar-inverse navbar-fixed-top" style="margin-bottom:0px;">
+		<div class="container">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			  </button>
+			  <a class="navbar-brand" href="#">Travelor</a>
+		       	  <ul class="nav navbar-nav navbar-right">
+			  <li><a href="#">Home</a></li>
+			  <li><a href="#">Category</a></li>
+			  <li><a href="#">Gallery</a></li>
+			  <li><a href="#">Advertisement</a></li>
+			  <li><a href="aboutus.php">About us</a></li>
+			  <li><a href="#">Contact</a></li>
+			</ul>
+		</div>
+			<div class="collapse navbar-collapse" id="myNavbar">
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="signup.html"><span class="glyphicon glyphicon-user"></span>Sign Up</a></li>
+					<li><a href="login.html"><span class="glyphicon glyphicon-log-in"></span> Log In</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
 	<div>
 		<div>
 			<img src="img/aboutusbanner.jpg" style="height: 700px; width: 100%;">
@@ -18,5 +44,12 @@
 			</div>
 		</div>
 	</div>
+	<footer>
+		<div class="container">
+			<center>
+				<p>Copyright &copy; Traveler. All Rights Reserved  |  Contact Us: +91 90000 00000</p>
+			</center>
+		</div>
+	</footer>
 </body>
 </html>
