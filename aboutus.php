3484bc0f456c69d4a4aff322a2793ef5840aed53
DEBJYOTI SAHA <roni19879@gmail.com>
2019-08-27 00:01:32 +0530

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>About Us</title>
+	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css">
+	<script type="text/javascript" src="bootstrap/js/jquery.js"></script>
+	<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+	<nav class="navbar navbar-inverse navbar-fixed-top" style="margin-bottom:0px;">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#">Travelor</a>
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="index.php">Home</a></li>
+          			<li><a href="#">Category</a></li>
+					<li><a href="#">Gallery</a></li>
+			        <li><a href="#">Advertisement</a></li>
+			        <li><a href="aboutus.php">About us</a></li>
+			        <li><a href="#">Contact</a></li>
+				</ul>
+			</div>
+			<div class="collapse navbar-collapse" id="myNavbar">
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="signup.html"><span class="glyphicon glyphicon-user"></span>Sign Up</a></li>
+					<li><a href="login.html"><span class="glyphicon glyphicon-log-in"></span> Log In</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+	<div>
+		<div>
+			<img src="img/aboutusbanner.jpg" style="height: 700px; width: 100%;">
+			<div>
+				<h1 style="font-size: 5.5em; position: absolute; top: 20%; left: 40%;color: white; ">About Us</h1>
+			</div>
+		</div>
+	</div><br><br><br>
+	<div class="container">
+		<div>
+			<h1 style="font-family: 'Roboto'; color: #d24105; margin: 40px 0 18px 0;">About Travelore</h1><br>
+			<h3 style="color: #434343; font-weight: 400; ">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur ab quas facilis obcaecati non ea, est odit repellat distinctio incidunt, quia aliquam eveniet quod deleniti impedit sapiente atque tenetur porro?</h2>
+		</div>
+		<br>
+		<ul style="list-style-type: none;">
+            <li><i class="glyphicon glyphicon-ok" style="font-size:20px;">        Vitae cumque eius modi expedita</i></li>
+            <li><i class="glyphicon glyphicon-ok" style="font-size:20px;">        Vitae cumque eius modi expedita</i></li>
+            <li><i class="glyphicon glyphicon-ok" style="font-size:20px;">        Vitae cumque eius modi expedita</i></li>
+
+
+        </ul>
+	</div>
+	<div class="container">
+		<div>
+			<h2 class="text-center">Our Team</h2>			
+		</div><br><br>
+          <div class="col-md-6 col-lg-4 text-center mb-5" data-aos="fade-up">
+            <img src="img/person_1.jpg" alt="Image" class="img-circle" style="height: 200px;width: 200px;">
+            <h2 class="text-black font-weight-light mb-4">Jean McMiller</h2>
+            <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur ab quas facilis obcaecati non ea, est odit repellat distinctio incidunt, quia aliquam eveniet quod deleniti impedit sapiente atque tenetur porro?</p>
+            <p>
+              <a href="#" class="pl-3 pr-3"> <i class="fa fa-twitter" style="font-size:24px">  </i>  </a>
+              <a href="#" class="pl-3 pr-3"><i class="fa fa-instagram" style="font-size:24px"></i>  </a>
+              <a href="#" class="pl-3 pr-3"><i class="fa fa-facebook" style="font-size:24px">  </i>  </a>
+            </p>
+          </div>
+          <div class="col-md-6 col-lg-4 text-center mb-5" data-aos="fade-up">
+            <img src="img/person_2.jpg" alt="Image" class="img-circle" style="height: 200px;width: 200px;">
+            <h2 class="text-black font-weight-light mb-4">Claire Peters</h2>
+            <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur ab quas facilis obcaecati non ea, est odit repellat distinctio incidunt, quia aliquam eveniet quod deleniti impedit sapiente atque tenetur porro?</p>
+            <p>
+              <a href="#" class="pl-3 pr-3"> <i class="fa fa-twitter" style="font-size:24px">  </i>  </a>
+              <a href="#" class="pl-3 pr-3"><i class="fa fa-instagram" style="font-size:24px"></i>  </a>
+              <a href="#" class="pl-3 pr-3"><i class="fa fa-facebook" style="font-size:24px">  </i>  </a>
+            </p>
+          </div>
+          <div class="col-md-6 col-lg-4 text-center mb-5" data-aos="fade-up">
+            <img src="img/person_4.jpg" alt="Image" class="img-circle" style="height: 200px;width: 200px;">
+            <h2 class="text-black font-weight-light mb-4">John Sawyer</h2>
+            <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur ab quas facilis obcaecati non ea, est odit repellat distinctio incidunt, quia aliquam eveniet quod deleniti impedit sapiente atque tenetur porro?</p>
+            <p>
+            <p>
+              <a href="#" class="pl-3 pr-3"> <i class="fa fa-twitter" style="font-size:24px">  </i>  </a>
+              <a href="#" class="pl-3 pr-3"><i class="fa fa-instagram" style="font-size:24px"></i>  </a>
+              <a href="#" class="pl-3 pr-3"><i class="fa fa-facebook" style="font-size:24px">  </i>  </a>
+            </p>
+          </div>
+	</div>
+	<footer>
+		<div class="container">
+			<center>
+				<p>Copyright &copy; Traveler. All Rights Reserved  |  Contact Us: +91 90000 00000</p>
+			</center>
+		</div>
+	</footer>
+	
+</body>
+</html>
---
+++ test.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>test</title>
+	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css">
+	<script type="text/javascript" src="bootstrap/js/jquery.js"></script>
+	<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="stylesheet.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+	<div>
+		<div>
+			<img src="img/aboutusbanner.jpg" style="height: 700px; width: 100%;">
+			<div>
+				<h1 style="font-size: 5.5em; position: absolute; top: 20%; left: 40%;color: white; ">About Us</h1>
+			</div>
+		</div>
+	</div><br><br><br>
+	<div class="container-fluid bg-grey">
+		<div>
+			<h1 style="font-family: 'Roboto'; color: #d24105; margin: 40px 0 18px 0;">About Travelore</h1><br>
+			<h3 style="color: #434343; font-weight: 400; ">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur ab quas facilis obcaecati non ea, est odit repellat distinctio incidunt, quia aliquam eveniet quod deleniti impedit sapiente atque tenetur porro?</h2>
+		</div>
+		<br>
+		<ul style="list-style-type: none;">
+            <li><i class="glyphicon glyphicon-ok" style="font-size:20px;">        Vitae cumque eius modi expedita</i></li>
+            <li><i class="glyphicon glyphicon-ok" style="font-size:20px;">        Vitae cumque eius modi expedita</i></li>
+            <li><i class="glyphicon glyphicon-ok" style="font-size:20px;">        Vitae cumque eius modi expedita</i></li>
+
+
+        </ul>
+	</div>
+	<div class="container">
+		<div>
+			<h2 class="text-center">Our Team</h2>			
+		</div><br><br>
+          <div class="col-md-6 col-lg-4 text-center mb-5" data-aos="fade-up">
+            <img src="img/person_1.jpg" alt="Image" class="img-circle" style="height: 200px;width: 200px;">
+            <h2 class="text-black font-weight-light mb-4">Jean McMiller</h2>
+            <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur ab quas facilis obcaecati non ea, est odit repellat distinctio incidunt, quia aliquam eveniet quod deleniti impedit sapiente atque tenetur porro?</p>
+            <p>
+              <a href="#" class="pl-3 pr-3"> <i class="fa fa-twitter" style="font-size:24px">  </i>  </a>
+              <a href="#" class="pl-3 pr-3"><i class="fa fa-instagram" style="font-size:24px"></i>  </a>
+              <a href="#" class="pl-3 pr-3"><i class="fa fa-facebook" style="font-size:24px">  </i>  </a>
+            </p>
+          </div>
+          <div class="col-md-6 col-lg-4 text-center mb-5" data-aos="fade-up">
+            <img src="img/person_2.jpg" alt="Image" class="img-circle" style="height: 200px;width: 200px;">
+            <h2 class="text-black font-weight-light mb-4">Claire Peters</h2>
+            <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur ab quas facilis obcaecati non ea, est odit repellat distinctio incidunt, quia aliquam eveniet quod deleniti impedit sapiente atque tenetur porro?</p>
+            <p>
+              <a href="#" class="pl-3 pr-3"> <i class="fa fa-twitter" style="font-size:24px">  </i>  </a>
+              <a href="#" class="pl-3 pr-3"><i class="fa fa-instagram" style="font-size:24px"></i>  </a>
+              <a href="#" class="pl-3 pr-3"><i class="fa fa-facebook" style="font-size:24px">  </i>  </a>
+            </p>
+          </div>
+          <div class="col-md-6 col-lg-4 text-center mb-5" data-aos="fade-up">
+            <img src="img/person_4.jpg" alt="Image" class="img-circle" style="height: 200px;width: 200px;">
+            <h2 class="text-black font-weight-light mb-4">John Sawyer</h2>
+            <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur ab quas facilis obcaecati non ea, est odit repellat distinctio incidunt, quia aliquam eveniet quod deleniti impedit sapiente atque tenetur porro?</p>
+            <p>
+            <p>
+              <a href="#" class="pl-3 pr-3"> <i class="fa fa-twitter" style="font-size:24px">  </i>  </a>
+              <a href="#" class="pl-3 pr-3"><i class="fa fa-instagram" style="font-size:24px"></i>  </a>
+              <a href="#" class="pl-3 pr-3"><i class="fa fa-facebook" style="font-size:24px">  </i>  </a>
+            </p>
+          </div>
+	</div>
+</body>
+</html>
