5699801d156f615066cccc7f14a1f834f0521eff
DEBJYOTI SAHA <roni19879@gmail.com>
2019-08-26 23:55:28 +0530

    Add files via upload

--- travel/aboutus.php
+++ travel/aboutus.php
@@ -42,12 +42,56 @@
 				<h1 style="font-size: 5.5em; position: absolute; top: 20%; left: 40%;color: white; ">About Us</h1>
 			</div>
 		</div>
+	</div><br><br><br>
+	<div class="container">
+		<div>
+			<h1 style="font-family: 'Roboto'; color: #d24105; margin: 40px 0 18px 0;">About Travelore</h1><br>
+			<h3 style="color: #434343; font-weight: 400; ">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur ab quas facilis obcaecati non ea, est odit repellat distinctio incidunt, quia aliquam eveniet quod deleniti impedit sapiente atque tenetur porro?</h2>
+		</div>
+		<br>
+		<ul style="list-style-type: none;">
+            <li><i class="glyphicon glyphicon-ok" style="font-size:20px;">        Vitae cumque eius modi expedita</i></li>
+            <li><i class="glyphicon glyphicon-ok" style="font-size:20px;">        Vitae cumque eius modi expedita</i></li>
+            <li><i class="glyphicon glyphicon-ok" style="font-size:20px;">        Vitae cumque eius modi expedita</i></li>
+
+
+        </ul>
 	</div>
 	<div class="container">
 		<div>
-			<h1 style="font-family: 'Roboto'; color: #d24105; margin: 40px 0 18px 0;">About Travelore</h1>
-			<h2 style="color: #434343; font-weight: 400; ">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur ab quas facilis obcaecati non ea, est odit repellat distinctio incidunt, quia aliquam eveniet quod deleniti impedit sapiente atque tenetur porro?</h2>
-		</div>
+			<h2 class="text-center">Our Team</h2>			
+		</div><br><br>
+          <div class="col-md-6 col-lg-4 text-center mb-5" data-aos="fade-up">
+            <img src="img/person_1.jpg" alt="Image" class="img-circle" style="height: 200px;width: 200px;">
+            <h2 class="text-black font-weight-light mb-4">Jean McMiller</h2>
+            <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur ab quas facilis obcaecati non ea, est odit repellat distinctio incidunt, quia aliquam eveniet quod deleniti impedit sapiente atque tenetur porro?</p>
+            <p>
+              <a href="#" class="pl-3 pr-3"> <i class="fa fa-twitter" style="font-size:24px">  </i>  </a>
+              <a href="#" class="pl-3 pr-3"><i class="fa fa-instagram" style="font-size:24px"></i>  </a>
+              <a href="#" class="pl-3 pr-3"><i class="fa fa-facebook" style="font-size:24px">  </i>  </a>
+            </p>
+          </div>
+          <div class="col-md-6 col-lg-4 text-center mb-5" data-aos="fade-up">
+            <img src="img/person_2.jpg" alt="Image" class="img-circle" style="height: 200px;width: 200px;">
+            <h2 class="text-black font-weight-light mb-4">Claire Peters</h2>
+            <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur ab quas facilis obcaecati non ea, est odit repellat distinctio incidunt, quia aliquam eveniet quod deleniti impedit sapiente atque tenetur porro?</p>
+            <p>
+              <a href="#" class="pl-3 pr-3"> <i class="fa fa-twitter" style="font-size:24px">  </i>  </a>
+              <a href="#" class="pl-3 pr-3"><i class="fa fa-instagram" style="font-size:24px"></i>  </a>
+              <a href="#" class="pl-3 pr-3"><i class="fa fa-facebook" style="font-size:24px">  </i>  </a>
+            </p>
+          </div>
+          <div class="col-md-6 col-lg-4 text-center mb-5" data-aos="fade-up">
+            <img src="img/person_4.jpg" alt="Image" class="img-circle" style="height: 200px;width: 200px;">
+            <h2 class="text-black font-weight-light mb-4">John Sawyer</h2>
+            <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur ab quas facilis obcaecati non ea, est odit repellat distinctio incidunt, quia aliquam eveniet quod deleniti impedit sapiente atque tenetur porro?</p>
+            <p>
+            <p>
+              <a href="#" class="pl-3 pr-3"> <i class="fa fa-twitter" style="font-size:24px">  </i>  </a>
+              <a href="#" class="pl-3 pr-3"><i class="fa fa-instagram" style="font-size:24px"></i>  </a>
+              <a href="#" class="pl-3 pr-3"><i class="fa fa-facebook" style="font-size:24px">  </i>  </a>
+            </p>
+          </div>
 	</div>
 	<footer>
 		<div class="container">
@@ -56,6 +100,6 @@
 			</center>
 		</div>
 	</footer>
-	<
+	
 </body>
 </html>
